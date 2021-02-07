e503d0c28afcc3d19bbfbc193f5fa1c0fcd13b3c
Tessa Cooper <55293851+tcooops@users.noreply.github.com>
2021-02-06 21:41:11 -0500

    adding express but needs to be fixed

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const http = require('http');
-//require is more or less the same as a js import
+const express = require('express');
+const path = require('path');
+const server = express();
 
-// const hostname = '127.0.0.1'; // this is localhost
+
+// set our view directory so express knows where to serve up our HTML: (note: views is kinda specific to an express app)
+server.set("views", path.join(__dirname, 'views'));
+
+// serve up any static assets from our base url and look in the public folder: (for css, images, scripts. etc)
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get("/", (req, res) => {
+    console.log('route route route for the home page, if u dont land its a shame');
+    res.sendFile('index');
+})
+
+server.get("/contact", (req ,res) => {
+    console.log('you have hit the contact route');
+    res.require('contact.html');
+})
+
+
 const port = process.env.PORT || 3000; // localhost:3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World'); // this is just a piece of txt we sent to browser
-});
 
 server.listen(port, () => {
   console.log(`Server is now running at ${port}/`);
